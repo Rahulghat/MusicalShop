e5fcf5e70aa8e5db030c94f01912c75ffee8ec27
Rahulghat <rahul.ghatvisawe@gmail.com>
2018-05-05 11:09:09 +0530

    Commit message

---
+++ src/main/java/com/me/MusicalShop/MusicalShopApplication.java
@@ -0,0 +1,12 @@
+package com.me.MusicalShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicalShopApplication.class, args);
+	}
+}
